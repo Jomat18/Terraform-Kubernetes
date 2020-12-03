226bcfec6734a30fb978e44d13b70381ff28fe7c
Jomat18 <manu.jose.torres@gmail.com>
2020-12-03 00:42:12 -0500

    doc: iniciando terraform

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "google" {
+  credentials = file("service-account.json")
+  project     = "terraform-297504"
+  region      = "us-central1"
+  zone        = "us-central1-c" 
+}
+
+resource "google_compute_instance" "my-cluster" {
+  name = "cluster-terraform"
+  machine_type = "f1-micro"
+  zone  = "us-central1-c" 
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"    
+      #image = "ubuntu-os-cloud/ubuntu-1604-lts"
+    }
+  }
+
+  network_interface {
+    # network = "default"  
+    network = google_compute_network.vpc_network.self_link    
+    access_config {
+    }
+  }
+
+  #metadata {
+  #  sshKeys = "test:${file("test.pub")}"
+  #}
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+  auto_create_subnetworks = "true"    
+}
